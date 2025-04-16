7335a4d7f8649311535df665c14a7f3963bf371d
Harshit Pandey <harshitpandey@jklu.edu.in>
2025-04-17 00:45:04 +0530

    change in api

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://vercel-backend-eta-five.vercel.app',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
