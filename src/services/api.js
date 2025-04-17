75318e8fa490a098a40134616b4737013458c2bf
Harshit Pandey <150131506+harshitpandey08@users.noreply.github.com>
2025-04-17 17:49:40 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://vercel-backend-umber-iota.vercel.app/api";
+const API_URL = "https://vercel-backend-gray-eta.vercel.app/api";
 
 // Create axios instance
 const api = axios.create({
